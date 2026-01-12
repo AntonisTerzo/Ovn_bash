d1b384722a2fd930e9cbed4fa8f9ab0ed56cc776
AntonisTerzo <antonis.terzopoulos@gmail.com>
2026-01-12 20:25:57 +0200

    fixed so that there is rest also

--- src/bash9.sh
+++ src/bash9.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-echo "Ange först talet:"
+echo "Mata in tal1:"
 read tal1
 
-echo "Ange andra talet:"
+echo "Mata in tal2:"
 read tal2
 
 addition=$(($tal1 + $tal2))
 subtraktion=$(($tal1 - $tal2))
 multiplikation=$(($tal1 * $tal2))
 division=$(echo "scale=2; $tal1 / $tal2" | bc)
+rest=$(($tal1 % $tal2))
 
 echo "$tal1 + $tal2 = $addition"
 echo "$tal1 - $tal2 = $subtraktion"
 echo "$tal1 * $tal2 = $multiplikation"
-echo "$tal1 / $tal2 = $division"
+echo "$tal1 / $tal2 = $division, rest $rest"
 
